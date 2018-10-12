049dc8e980ce27d1318df20e6efa27acc8624236
Christopher Beltran <christod.beltran@gmail.com>
2018-10-12 04:03:33 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,2 @@
-INSERT INTO bears (name, age, gender, color)
-    VALUES ("John", 11, 3.4, 3);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Mr.Chocolate", 11, "male", "brown", "calm", "" );
